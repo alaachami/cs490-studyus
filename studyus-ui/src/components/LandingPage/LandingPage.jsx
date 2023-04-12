d7ef8c331000e11f04b7930d37fe849d4c172d11
panwaffle36 <ajg86@njit.edu>
2023-04-11 22:05:24 -0400

    Coded links for landing page

--- studyus-ui/src/components/LandingPage/LandingPage.jsx
+++ studyus-ui/src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./LandingPage.css";
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
 	return (
+        <>
         <h1>Landing page</h1>
-        // DO HTML CODING HERE
+        <h2>
+        <Link to="/register">Click here to sign up!</Link>
+        </h2>
+        <h2>
+        <Link to="/login">Click here to log in!</Link>
+        </h2>
+        </>
 	);
 }
