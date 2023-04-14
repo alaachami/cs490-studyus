96a4c8337199e7577b727c84d6660ade6d28eed0
alaachami <alaachami916@gmail.com>
2023-04-14 18:22:41 -0400

    server chat setup

--- studyus-api/server.js
+++ studyus-api/server.js
@@ -1,8 +1,32 @@
 const app = require('./app')
 const {PORT} = require("./config")
 
+//create server on port 3000
+const io = require('socket.io')(3000, {
+    cors: {
+      origin: '*',
+    }
+})
 //const PORT = process.env.PORT || 3001
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server listening at http://localhost:${PORT}`)
+
+
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
 })
+
+// app.listen(PORT, () => {
+//     console.log(`🚀 Server listening at http://localhost:${PORT}`)
+// })
