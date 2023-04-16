4b6923cb802e9e8022481b33393b8d4d9c31c451
alaachami <alaachami916@gmail.com>
2023-04-15 23:31:12 -0400

    video call functionality

--- studyus-api/app.js
+++ studyus-api/app.js
@@ -17,26 +17,35 @@ const app = express()
 
 //Server Socket.io
 const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4:uuiV4 } = require('uuid')
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
+})
+const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req,res) => {
-    res.redirect(`/${uuiV4()}`)
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
 })
 
-app.get('/:room', (req,res) =>{
-    res.render('room', { roomId: req.params.room})
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
 })
 
 io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected', userId)
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
+  })
 })
+
 //APP USE - Parse incoming request bodies
 app.use(cors())
 app.use(express.json())
--- studyus-api/server.js
+++ studyus-api/server.js
@@ -4,14 +4,13 @@ const {PORT} = require("./config")
 const { v4:uuiV4 } = require('uuid')
 
 const server = require('http').Server(app)
-const io = require('socket.io')(server)
-//const PORT = process.env.PORT || 3001
-io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected', userId)
-    })
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
 })
+//const PORT = process.env.PORT || 3001
+
 
 server.listen(PORT, () => {
     console.log(`🚀 Server listening at http://localhost:${PORT}`)
