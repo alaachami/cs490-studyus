7037f14543f1d527224dcaa5e8c94968552ffc43
thotran <thotran.exe@gmail.com>
2023-04-16 01:57:15 -0400

    init

--- studyus-ui/src/components/LoginPage/LoginPage.jsx
+++ studyus-ui/src/components/LoginPage/LoginPage.jsx
@@ -4,9 +4,13 @@ import LoginForm from "./Loginform/LoginForm";
 
 export default function LoginPage() {
         return (
+        <>
+        <div className="wallpaper"></div>
         <div className="login-page">
-                <h1>Login Page</h1>
+                <h1>Welcome Back</h1>
                 <LoginForm />
         </div>
+        <div className="stamp"><h1>StudyUs</h1></div>
+        </>
         );
 }
