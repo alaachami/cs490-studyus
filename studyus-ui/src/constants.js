07377410b56ecd9a1355c5051239cfb6c6fce6d3
alaachami <alaachami916@gmail.com>
2023-04-18 18:23:06 -0400

    update constants.js

--- studyus-ui/src/constants.js
+++ studyus-ui/src/constants.js
@@ -1,11 +1,11 @@
-//const PRODUCTION_API_BASE_URL = (Insert deployed backend link here);
+const PRODUCTION_API_BASE_URL = "https://studyus.herokuapp.com";
 const DEVELOPMENT_API_BASE_URL = "http://localhost:3001";
 // const API_BASE_URL =
 //   process.env.NODE_ENV == "production"
 //     ? PRODUCTION_API_BASE_URL
 //     : DEVELOPMENT_API_BASE_URL;
 // const API_BASE_URL =PRODUCTION_API_BASE_URL
-const API_BASE_URL=DEVELOPMENT_API_BASE_URL
+const API_BASE_URL=PRODUCTION_API_BASE_URL
 //   process.env.NODE_ENV == "production"
 //     ? PRODUCTION_API_BASE_URL
 //     : DEVELOPMENT_API_BASE_URL;
