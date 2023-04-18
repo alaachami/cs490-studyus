16c72f539f40865901f21cf713dc866d01f74b5f
alaachami <alaachami916@gmail.com>
2023-04-18 18:38:27 -0400

    update server.js

--- studyus-api/server.js
+++ studyus-api/server.js
@@ -1,7 +1,12 @@
 const app = require('./app')
 const {PORT} = require("./config")
+const cors = require('cors');
 
 //const PORT = process.env.PORT || 3001
+
+app.use(cors({
+    origin: "*"
+}));
 
 app.listen(PORT, () => {
     console.log(`🚀 Server listening at http://localhost:${PORT}`)
