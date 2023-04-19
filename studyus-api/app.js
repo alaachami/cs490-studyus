bafa405057dc2ce3ec836e66d8c16e02da354306
alaachami <alaachami916@gmail.com>
2023-04-19 16:36:34 -0400

    update CORS options

--- studyus-api/app.js
+++ studyus-api/app.js
@@ -16,9 +16,13 @@ const callRoutes = require('./routes/call')
 const app = express()
 
 //APP USE - Parse incoming request bodies
-app.use(cors({
-    origin: "*",
-}))
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
+
 app.use(express.json())
 app.use(morgan('tiny'))
 //APP USE - Security Middleware to authenticate user and create JWTs
