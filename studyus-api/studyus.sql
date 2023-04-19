b715fa7f52a2d498fbb314988e54687bd544280e
alaachami <alaachami916@gmail.com>
2023-04-18 21:28:19 -0400

    update db name

--- studyus-api/studyus.sql
+++ studyus-api/studyus.sql
@@ -2,9 +2,9 @@
 \prompt "Return for yes or control-C to cancel > " answer
 
 -- DROPPING AND RECREATING THE STUDYUS DATABASE
-DROP DATABASE studyus;
-CREATE DATABASE studyus;
+DROP DATABASE d1gj6ej3leju7j;
+CREATE DATABASE d1gj6ej3leju7j;
 
 --CONNECTING TO THE STUDYUS DATABASE AND STUDYUS SCHEMA TABLES
-\connect studyus
+\connect d1gj6ej3leju7j
 \i studyus-schema.sql
