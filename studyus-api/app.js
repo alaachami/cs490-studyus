1a3fa26444699d921d8b7ec3f97771d8bf787b3a
alaachami <alaachami916@gmail.com>
2023-04-21 21:32:02 -0400

    debug on prod

--- studyus-api/app.js
+++ studyus-api/app.js
@@ -17,16 +17,14 @@ const app = express()
 
 //APP USE - Parse incoming request bodies
 const corsOptions ={
-    origin: '*',  // 'http://localhost:5173', 
-    credentials:true,            //access-control-allow-credentials:true
+    origin: '*',  // 'http://localhost:5173'
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions))
 
 app.use(express.json())
 
-// Serve the frontend files from the dist directory
-app.use(express.static('../studyus-ui/dist'));
+
 app.use(morgan('tiny'))
 //APP USE - Security Middleware to authenticate user and create JWTs
 app.use(security.extractUserFromJwt)
